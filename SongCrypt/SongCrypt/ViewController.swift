2440fbb1c9e99703a7d7a1346272edf2af6c32f8
Ryan Sullivan <rps214@verizon.net>
2017-01-20 23:39:05 -0500

    Initial commit

---
+++ SongCrypt/SongCrypt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SongCrypt
+//
+//  Created by Ryan Sullivan on 1/20/17.
+//  Copyright © 2017 Ryan Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
