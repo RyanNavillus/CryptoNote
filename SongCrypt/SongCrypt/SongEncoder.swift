d8bd7f37ee5137757ed6242ebc20b0c4c73507ee
Ryan Sullivan <rps214@verizon.net>
2017-01-21 00:02:43 -0500

    Added Superpowered SDK

---
+++ SongCrypt/SongCrypt/SongEncoder.swift
@@ -0,0 +1,15 @@
+//
+//  SongEncoder.swift
+//  SongCrypt
+//
+//  Created by Ryan Sullivan on 1/20/17.
+//  Copyright © 2017 Ryan Sullivan. All rights reserved.
+//
+
+import Foundation
+class songEncoder: NSObject{
+    
+    
+    
+    
+}
